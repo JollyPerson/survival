4b1d9f263d205423d634b62caf27ffdcb26e9017
jollyperson <jollyperson_real@outlook.com>
2021-08-16 22:13:29 +0100

    packets

---
+++ Assets/Scripts/network/Packet/MessagePacketData.cs
@@ -0,0 +1,9 @@
+﻿public class MessagePacketData : PacketData
+{
+    public string Message { get; set; }
+
+    public MessagePacketData(string data)
+    {
+        Message = data;
+    }
+}
---
+++ Assets/Scripts/network/Packet/Packet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using KaymakNetwork;
+public class Packet
+{
+    public PacketType Type { get; set; }
+    public PacketData Data { get; set; }
+    
+
+    public static Packet Deserialize(IEnumerable<byte> data)
+    {
+        var byteData = data.Skip(sizeof(int))
+                .Decrypt()
+                .Decompress()
+                .ToArray();
+
+        var content = Encoding.ASCII.GetString(byteData);
+        Console.WriteLine(content);
+        Packet packet = JsonConvert.DeserializeObject<Packet>(content);
+        return packet;
+    }
+
+    public byte[] Serialize()
+    {
+        string data = JsonConvert.SerializeObject(this);
+
+        byte[] byteData = Encoding.ASCII.GetBytes(data)
+                    .Compress()
+                    .Encrypt()
+                    .PrependLength()
+                    .ToArray();
+        
+        return byteData;
+    }
+}
+
+
